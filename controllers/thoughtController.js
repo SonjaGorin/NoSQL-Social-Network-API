ceda2826c839d301949164ef052aa683c9653071
Sonja <sonjaljatkovic@gmail.com>
2024-01-02 16:40:13 -0500

    added getSingleThought function

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -9,4 +9,18 @@ module.exports = {
             res.status(500).json(err);
         }
     },
+
+    async getSingleThought(req, res) {
+        try {
+            const thought = await Thought.findOne({ _id: req.params.thoughtId });
+        
+            if (!thought) {
+                return res.status(404).json({ message: "No thought found with that id" });
+            }
+        
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
 }
