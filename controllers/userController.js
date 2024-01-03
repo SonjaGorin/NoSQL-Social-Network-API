d2ea2a6adac15e4bce94fef512ba29560c5e35e9
Sonja <sonjaljatkovic@gmail.com>
2024-01-02 19:56:38 -0500

    added index.js in routes

---
+++ controllers/userController.js
@@ -0,0 +1,26 @@
+const { User, Thought } = require("../models");
+
+module.exports = {
+    async getAllUsers(req, res) {
+        try {
+            const user = await User.find();
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async getSingleThought(req, res) {
+        try {
+            const user = await User.findOne({ _id: req.params.userId });
+        
+            if (!user) {
+                return res.status(404).json({ message: "No user found with that id" });
+            }
+        
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+}                                                         
