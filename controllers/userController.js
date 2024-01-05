f9af70674420fd5a19efce8bb2c3839dae15a86b
Sonja <sonjaljatkovic@gmail.com>
2024-01-05 00:12:27 -0500

    added screenshots

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,7 +19,7 @@ module.exports = {
             const user = await User.findOne({ _id: req.params.id })
             .populate("thoughts")
             .populate("friends")
-            .select('-__v');
+            .select("-__v");
         
             if (!user) {
                 return res.status(404).json({ message: "No user found with that id" });
